4af376a48d0677d95eb31cacdb6a53298a658021
Pavel Rebrov <pavel@rebrov.ru>
2014-09-28 22:18:29 +0800

    Question 2

--- week4-quiz.R
+++ week4-quiz.R
@@ -9,3 +9,16 @@ data <- read.csv(fileName)
 strsplit(names(data), "\\wgtp")[123]
 
 ### ANSWER 1. [1] ""   "15"
+
+### QUESTION 2. Remove the commas from the GDP numbers in millions of dollars and average them. What is the average? 
+
+gdpURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+gdpFile <- "./data/GDP.csv"
+download.file(gdpURL, gdpFile, method = "curl")
+
+gdpData <- read.csv(gdpFile, skip = 5, nrows = 190, stringsAsFactors = F, header = F)
+
+## replace commas with nothing with gsub, convert to numeric and calculate mean
+mean(as.numeric(gsub(",", "", gdpData$V5)))
+
+### ANSWER 2. [1] 377652.4
