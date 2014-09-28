4123a8c9ac60e71e3ea45818fc997b1fdd94b259
Pavel Rebrov <pavel@rebrov.ru>
2014-09-29 07:53:00 +0800

    Question 5

--- week4-quiz.R
+++ week4-quiz.R
@@ -44,3 +44,19 @@ mergedData <- merge(gdpData, eduData, as.x = "CountryCode", as.y = "CountryCode"
 length(grep("[Ff]iscal year end(.*)+June", mergedData$Special.Notes))
 
 ### ANSWER 4. [1] 13
+
+### QUESTION 5. How many values were collected in 2012? How many values were collected on Mondays in 2012?
+
+## You can use the quantmod (http://www.quantmod.com/) package to get historical stock prices for publicly traded companies on the NASDAQ and NYSE. Use the following code to download data on Amazon's stock price and get the times the data was sampled.
+library(lubridate)
+library(quantmod)
+amzn = getSymbols("AMZN", auto.assign=FALSE)
+sampleTimes = index(amzn)
+
+sampleTimes = ymd(sampleTimes)round_date(sampleTimes, "year")
+
+## Subset observations made in 2012
+Y2012 <- subset(sampleTimes, year(sampleTimes) == 2012)
+length(Y2012) ## [1] 250
+## Find out number of Mondays in this subset
+length(which(wday(Y2012, label = T) == "Mon")) ## [1] 47
